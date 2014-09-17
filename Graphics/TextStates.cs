a402a35cb559f3d8f01d44e8f49db5d2b4d1326b
Tim Stoddard <info@gamepopper.co.uk>
2014-09-17 01:20:42 +0100

    Maybe Later...

---
+++ Graphics/TextStates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ricoh2DFramework.Graphics
+{
+    public enum TextAlignment
+    {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+}
