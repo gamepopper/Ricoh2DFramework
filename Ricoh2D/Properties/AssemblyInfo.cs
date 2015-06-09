82f20598ff588c049422c2cec62fc8a8c900c50b
Tim Stoddard <info@gamepopper.co.uk>
2015-06-09 23:38:52 +0100

    Ported entire project to new XNA-less MonoGame 3.4

--- Ricoh2D/Properties/AssemblyInfo.cs
+++ Ricoh2D/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ricoh2DFramework")]
-[assembly: AssemblyProduct("Ricoh2DFramework")]
-[assembly: AssemblyDescription("Ricoh2D is a simple 2D framework to use with XNA/Monogame.")]
-[assembly: AssemblyCompany("Gamepopper")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTitle("Ricoh2D")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ricoh2D")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("55b0ceac-a6a3-4bfc-a817-5a2794fb1d47")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8f5d7a0-2678-4aa0-8481-f362eabc2cfe")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
